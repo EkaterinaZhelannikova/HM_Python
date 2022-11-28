dbd07677944c603bbfc7b64f5eedd978f728f8b3
Ekaterina Zhelannikova <katya.zhelannikova@mail.ru>
2022-11-28 18:27:14 +0300

    save

--- Sem_2/task_1.py
+++ Sem_2/task_1.py
@@ -11,7 +11,6 @@ my_str = 'cat'
 my_list = [3, 'wow', 1.6]
 my_dict = {'Hello': 'Привет'}
 my_tuple = (1, 'cat', 2.34)
-a = None
-final_list = [my_int, my_float, my_str, my_list, my_dict, my_tuple, a]
+final_list = [my_int, my_float, my_str, my_list, my_dict, my_tuple]
 for el in final_list:
     print(f'{el} is {type(el)}')
